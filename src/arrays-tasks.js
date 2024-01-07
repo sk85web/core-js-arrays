aefd3d8a6d9f122b53b328300417baac6caa9f41
Sergey <sk85post@gmail.com>
2024-01-07 10:24:34 +0300

    feat: complete calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -339,8 +339,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) return 0;
+  const div = arr.flatMap((el) => el[0] - el[1]);
+  return div[0] + div[1];
 }
 
 /**
