6ebac7ff05312cbf77b11970126f25dc83fca5da
Sergey <sk85post@gmail.com>
2024-01-07 12:17:22 +0300

    feat: complete generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -379,8 +379,10 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const newArr = Array.from({ length: len }).fill(1);
+  const result = newArr.map((el, index) => 2 * index + el);
+  return result;
 }
 
 /**
