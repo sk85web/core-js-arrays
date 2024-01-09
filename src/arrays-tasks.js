6122474003af648aaf8fd3cde9c3f0ca7bda6283
Sergey <sk85post@gmail.com>
2024-01-09 20:08:48 +0300

    feat: complete findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -521,8 +521,16 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const result = [];
+  const commonArr = arr1.concat(arr2);
+  commonArr.map((el, index) => {
+    if (commonArr.indexOf(el) !== index) {
+      result.push(el);
+    }
+    return el;
+  });
+  return result;
 }
 
 /**
